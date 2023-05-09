2e7ad6f016af2ceb96d496806874d1f0c3f567e5
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-05-09 16:35:09 -0300

    Update quest03.py

--- quest03.py
+++ quest03.py
@@ -1,56 +1,79 @@
 class No:
-    def __init__(self,valor):
+    def __init__(self, valor):
         self.valor = valor
         self.proximo = None
 
 class Pilha:
     def __init__(self):
-        self.topo = None
+        self._topo = None
         self.tamanho = 0
-
+   
     def __len__(self):
         return self.tamanho
-
+   
     def is_empty(self):
         return self.tamanho == 0
-
+   
     def inserir(self, valor):
-        No = No(valor)
-        No.proximo = self.topo
-        self.topo = No
+        no = No(valor)
+        no.proximo = self._topo
+        self._topo = no
         self.tamanho += 1
-
+   
     def remover(self):
         if self.is_empty():
             raise IndexError("A pilha está vazia")
-        valor = self.topo.valor
-        self.topo = self.topo.proximo
-        self.tamanho -+ 1
+        valor = self._topo.valor
+        self._topo = self._topo.proximo
+        self.tamanho -= 1
         return valor
-
-    def _top(self):
-        if self.size == 0:
-            raise Exception("A pilha está vazia")
-        return self.top.valor
+   
+    def topo(self):
+        if self.is_empty():
+            raise IndexError("A pilha está vazia")
+        return self._topo.valor
+    
+  def infixa_para_posfixa(expressao):
+    precendencia = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+    operadores = Pilha()
+    posfixa = []
+    numeros = '0123456789'
+    for caracter in expressao:
+        if caracter in numeros:
+            posfixa.append(caracter)
+        elif caracter in precendencia:
+            while not operadores.is_empty() \
+            and operadores.topo() != '(' \
+            and precendencia[caracter] <= precendencia[operadores.topo()]:
+                posfixa.append(operadores.remover())
+            operadores.inserir(caracter)
+    while not operadores.is_empty():
+        posfixa.append(operadores.remover())
+    return ''.join(posfixa)
 
 def calcular(exp):
-    p = pilha()
-    for caractere in exp:
-        if caractere.isdigit():
-            p.insert(caractere)
+    p = Pilha()
+    for caracter in exp:
+        if caracter.isdigit():
+            p.inserir(caracter)
         else:
             num2 = p.remover()
             num1 = p.remover()
-            if caractere == '+':
-                resultado = int(nume1) + int(num2)
+            if caracter == '+':
+                resultado = int(num1) + int(num2)
                 p.inserir(str(resultado))
-            elif caractere == '-':
+            elif caracter == '-':
                 resultado = int(num1) - int(num2)
-                p.insert(str(resultado))
-            elif caractere == '*':
+                p.inserir(str(resultado))
+            elif caracter == '*':
                 resultado = int(num1) * int(num2)
-                p.insert(str(resultado))
-            elif caractere == '/':
+                p.inserir(str(resultado))
+            elif caracter == '/':
                 resultado = int(num1) / int(num2)
-                p.insert(str(resultado))
+                p.inserir(str(resultado))
     return p.remover()
+
+exp = input('Digite uma expressão: ')
+pos = infixa_para_posfixa(exp)
+resposta = calcular(pos)
+print(resposta)
