f456b4e01c68ba7958828134b1fe6bf411fb4545
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-05-09 16:31:08 -0300

    Update quest10.py

--- quest10.py
+++ quest10.py
@@ -32,3 +32,54 @@ class Pilha:
         if self.is_empty():
             raise IndexError("A pilha está vazia")
         return self._topo.valor
+    
+  def infixa_para_posfixa(expressao):
+    precendencia = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+    operadores = Pilha()
+    posfixa = []
+    numeros = '0123456789'
+    for caracter in expressao:
+        if caracter in numeros:
+            posfixa.append(caracter)
+        elif caracter == '(':
+            operadores.inserir(caracter)
+        elif caracter == ')':
+            while operadores.topo() != '(':
+                posfixa.append(operadores.remover())
+            operadores.remover()
+        elif caracter in precendencia:
+            while not operadores.is_empty() \
+            and operadores.topo() != '(' \
+            and precendencia[caracter] <= precendencia[operadores.topo()]:
+                posfixa.append(operadores.remover())
+            operadores.inserir(caracter)
+    while not operadores.is_empty():
+        posfixa.append(operadores.remover())
+    return ''.join(posfixa)
+
+def calcular(exp):
+    p = Pilha()
+    for caracter in exp:
+        if caracter.isdigit():
+            p.inserir(caracter)
+        else:
+            num2 = p.remover()
+            num1 = p.remover()
+            if caracter == '+':
+                resultado = int(num1) + int(num2)
+                p.inserir(str(resultado))
+            elif caracter == '-':
+                resultado = int(num1) - int(num2)
+                p.inserir(str(resultado))
+            elif caracter == '*':
+                resultado = int(num1) * int(num2)
+                p.inserir(str(resultado))
+            elif caracter == '/':
+                resultado = int(num1) / int(num2)
+                p.inserir(str(resultado))
+    return p.remover()
+
+exp = input('Digite uma expressão: ')
+pos = infixa_para_posfixa(exp)
+resposta = calcular(pos)
+print(resposta)
